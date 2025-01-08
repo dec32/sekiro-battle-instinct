5790927ba7b7f95061a3788cf142c707c3294352
dec32 <1064103102@qq.com>
2025-01-08 22:19:36 +0800

    Separate different languages in configuration.

--- build.ps1
+++ build.ps1
@@ -4,9 +4,16 @@ param (
 
 if ($Release) {
     cargo build --release
-    Copy-Item ".\target\release\battle_instinct.dll" -Destination ".\dinput8.dll"
-    Compress-Archive -Update -LiteralPath @(".\battle_instinct.cfg", ".\dinput8.dll") -CompressionLevel "NoCompression" -DestinationPath ".\battle-instinct_x64.zip"
-    Remove-Item .\dinput8.dll
+    New-Item -ItemType Directory -Force -Path ".\tmp"
+    Copy-Item ".\target\release\battle_instinct.dll" -Destination ".\tmp\dinput8.dll"
+
+    Copy-Item -Force ".\battle_instinct.cfg" -Destination ".\tmp\battle_instinct.cfg"
+    Compress-Archive -Update -LiteralPath @(".\tmp\battle_instinct.cfg", ".\tmp\dinput8.dll") -CompressionLevel "NoCompression" -DestinationPath ".\battle-instinct_x64.zip"
+
+    Copy-Item -Force ".\battle_instinct_zh.cfg" -Destination ".\tmp\battle_instinct.cfg"
+    Compress-Archive -Update -LiteralPath @(".\tmp\battle_instinct.cfg", ".\tmp\dinput8.dll") -CompressionLevel "NoCompression" -DestinationPath ".\battle-instinct_zh_x64.zip"
+    
+    Remove-Item -Recurse ".\tmp"
 } else {
     cargo build
     Copy-Item ".\target\debug\battle_instinct.dll" -Destination "C:\Program Files (x86)\Steam\steamapps\common\Sekiro\dinput8.dll"
