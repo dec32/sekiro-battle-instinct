c305189209c83bf13565570c10c72d44e0cfa0ed
dec32 <1064103102@qq.com>
2025-04-04 20:16:11 +0800

    Update release script

--- release.ps1
+++ release.ps1
@@ -1,3 +1,4 @@
 .\build -Release
+git tag -d nightly
 git push --delete origin nightly
 gh release create nightly ".\battle-instinct.zip", ".\battle-instinct_zh.zip" -t "Nightly Build" -n "Nightly Build"
