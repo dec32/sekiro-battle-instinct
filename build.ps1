bac54b19d1e0ee812109adcbd65955c6ebb34c9c
dec32 <56337978+dec32@users.noreply.github.com>
2026-03-06 23:27:21 +0800

    chore: build script

--- build.ps1
+++ build.ps1
@@ -4,9 +4,9 @@ param (
 
 if ($Release) {
     cargo build --release
-    Copy-Item ".\target\release\battle_instinct.dll" -Destination "C:\Program Files (x86)\Steam\steamapps\common\Sekiro\dinput8.dll"
+    Copy-Item ".\target\release\sekiro_battle_instinct.dll" -Destination "C:\Program Files (x86)\Steam\steamapps\common\Sekiro\dinput8.dll"
     New-Item -ItemType Directory -Force -Path ".\tmp"
-    Copy-Item ".\target\release\battle_instinct.dll" -Destination ".\tmp\dinput8.dll"
+    Copy-Item ".\target\release\sekiro_battle_instinct.dll" -Destination ".\tmp\dinput8.dll"
 
     Copy-Item -Force ".\res\battle_instinct.cfg" -Destination ".\tmp\battle_instinct.cfg"
     Compress-Archive -Update -LiteralPath @(".\tmp\battle_instinct.cfg", ".\tmp\dinput8.dll") -CompressionLevel "Optimal" -DestinationPath ".\battle-instinct.zip"
@@ -17,6 +17,6 @@ if ($Release) {
     Remove-Item -Recurse ".\tmp"
 } else {
     cargo build
-    Copy-Item ".\target\debug\battle_instinct.dll" -Destination "C:\Program Files (x86)\Steam\steamapps\common\Sekiro\dinput8.dll"
+    Copy-Item ".\target\debug\sekiro_battle_instinct.dll" -Destination "C:\Program Files (x86)\Steam\steamapps\common\Sekiro\dinput8.dll"
 }
 
